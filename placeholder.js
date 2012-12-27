252c1d2e5374eeec95d4ef00df0fe9bce52a964c
Alex Vakhov <alex.vakhov@gmail.com>
2012-12-27 10:10:04 +0600

    add gitignore

--- placeholder.js
+++ placeholder.js
@@ -1,6 +1,6 @@
 (function($){
 jQuery.fn.Placeholder = function(){
-	var inputs = $(this).find("[type=text],[type=email],[type=password]");
+	var inputs = $(this).find(":text,[type=email],:password");
 	inputs.each(function(){
 		if($(this).data('label')){
 			var labelName = $(this).data('label');
@@ -8,7 +8,7 @@ jQuery.fn.Placeholder = function(){
 			var labelName = ($(this).attr('name')) ? $(this).attr('name') : $(this).attr('type');
 		}
 		$(this).wrap('<div class="placeholderWrapper" style="position:relative;"></div>').parent().prepend('<label style="cursor: text;padding-left:3px;padding-top:1px;position:absolute;text-transform:uppercase;z-index:3;">'+labelName+'</label>');
-		
+
 		$(this).focus(function(){
 			$(this).siblings("label").hide();
 		});
