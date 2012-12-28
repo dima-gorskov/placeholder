b2393c2f996d92cf7115dd05fd48d7d50959d728
Dima Gorskov <dima@doejo.com>
2012-12-28 12:14:14 +0600

    style in head

--- placeholder.js
+++ placeholder.js
@@ -1,13 +1,14 @@
 (function($){
 	jQuery.fn.Placeholder = function(){
 		var inputs = $(this).find(":text,[type=email],:password,textarea");
+		$("title").after("<style type='text/css'>.placeholderWrapper{position:relative;}.placeholderWrapper>label{cursor: text;padding-left:3px;padding-top:1px;position:absolute;text-transform:uppercase;z-index:3;}</style>");
 		inputs.each(function(){
 			if($(this).data('label')){
 				var labelName = $(this).data('label');
 			} else {
 				var labelName = ($(this).attr('name')) ? $(this).attr('name') : $(this).attr('type');
 			}
-			$(this).wrap('<div class="placeholderWrapper" style="position:relative;"></div>').parent().prepend('<label style="cursor: text;padding-left:3px;padding-top:1px;position:absolute;text-transform:uppercase;z-index:3;">'+labelName+'</label>');
+			$(this).wrap('<div class="placeholderWrapper"></div>').parent().prepend('<label>'+labelName+'</label>');
 
 			$(this).focus(function(){
 				$(this).prev().hide();
